00b693a6df73742bcc45f83d546c8f97f35845e6
Salsabila Azaria <salsabilaazariaa@gmail.com>
2021-03-05 15:01:34 +0700

    commit

---
+++ SocialMediaOnboardingProject/VIew/ActionNode.swift
@@ -0,0 +1,9 @@
+//
+//  ActionNode.swift
+//  SocialMediaOnboardingProject
+//
+//  Created by Salsabila Azaria on 3/5/21.
+//  Copyright © 2021 salsabilaazaria. All rights reserved.
+//
+
+import Foundation
---
+++ SocialMediaOnboardingProject/VIew/ActionPost.swift
@@ -0,0 +1,9 @@
+//
+//  ActionPost.swift
+//  SocialMediaOnboardingProject
+//
+//  Created by Salsabila Azaria on 3/5/21.
+//  Copyright © 2021 salsabilaazaria. All rights reserved.
+//
+
+import Foundation
